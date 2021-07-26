eb02293f577037f0779848412e41a938be686b11
Łukasz Okoń <l.okon@haso.pl>
2021-07-26 18:14:01 +0200

    in user.py: -add User class constructor -add send_message method in User class -add print_messages method in User class

--- user.py
+++ user.py
@@ -1,3 +1,14 @@
-
 class User:
-    pass
+    def __init__(self, nickname, password):
+        self.nickname = nickname
+        self.password = password
+        self.messages = []
+
+    def send_message(self, nickname_to, text):
+        pass
+
+    def print_messages(self):
+        for message in self.messages:
+            print(f'*    {message.from_user} - {message.timestamp}')
+            print(message.text)
+            print()
